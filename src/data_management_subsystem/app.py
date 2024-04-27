d5fbd6d6dab8b9b0cd08f2b300b4aa7480762b0e
David Chavarro <david.e.chavarro@outlook.com>
2024-04-27 12:18:29 -0400

    Update app.py

--- src/data_management_subsystem/app.py
+++ src/data_management_subsystem/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, session, json
 from flask_cors import CORS, cross_origin
 from .chat_data_module import ChatData as cd
+from .user_account_management import UserAccountManagement as uam
 from .NonExistentUserException import NonExistentUserException
 from datetime import datetime as dt
 
@@ -12,6 +13,99 @@ import requests
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type/json'
+
+# API Calls regarding User Account Management
+@app.route('/create_user', methods=['POST'])
+@cross_origin()
+def create_user():
+    """This API call handles the creatiion of a new user.
+    
+    PREQUISITES - The DOB must follow the 'MM/DD/YYYY' format.
+    """
+    try:
+        json_data = request.get_json(silent=True)
+        username = json_data["username"]
+        email = json_data["email"]
+        password = json_data["password"]
+        dob = dt.strptime(json_data["dob"], "%m/%d/%Y")
+        if "share_conv_for_training" in json_data.keys():
+            share_conv_for_training = json_data["share_conv_for_training"]
+        else:
+            share_conv_for_training = False
+
+        uam_object = uam()
+        uam_object.create_user(username, dob, email, password, share_conv_for_training)
+        newUserID = uam_object.userId
+        return json.dumps({'success': True, 'user_id': newUserID}), 201
+    except Exception as e:
+        traceback.print_exc()
+        return json.dumps({'success': False, 'exception_type': str(type(e).__name__), 'exception_details': str(e)}), 500
+    
+@app.route('/generate_verification_code/mail/<user_id>', methods=['PUT'])
+@cross_origin()
+def generate_verification_code(user_id):
+    try:
+        uam_object = uam(int(user_id))
+        correct_code = uam_object.generate_2fa_code(app)
+        return json.dumps({'success': True}), 200
+    except Exception as e:
+       return json.dumps({'success': False, 'exception_type': str(type(e).__name__), 'exception_details': str(e)}), 500 
+    
+
+@app.route('/authenticate_verification_code/<user_id>', methods=['POST'])
+@cross_origin()
+def authenticate_verification_code(user_id):
+    try:
+        json_data = request.get_json(silent=False)
+        code = int(json_data['code'])
+        uam_object = uam(int(user_id))
+        verif_code_correct = uam_object.verify_2fa_code(code)
+        if verif_code_correct:
+            return json.dumps({'success': True}), 200
+        else:
+            return json.dumps({'success': False, 'exception_type': "InvalidCodeException", 
+                'exception_details': "The verification code is incorrect or expired."}), 401
+    except Exception as e:
+        return json.dumps({'success': False, 'exception_type': str(type(e).__name__), 'exception_details': str(e)}), 500     
+
+
+@app.route('/login', methods=['POST'])
+@cross_origin()
+def login_user():
+    """This function handles login requests by verifying the user's email and password."""
+    try:
+        json_data = request.get_json(silent=False)
+        email = json_data['email']
+        password = json_data['password']
+        uam_object = uam()
+        uam_object.login_user(email, password)
+        user_id = uam_object.userId
+        return json.dumps({'success': True, 'user_id': user_id}), 200
+    except ValueError as ve:
+        return json.dumps({'success': False, 'exception_type': str(type(ve).__name__), 'exception_details': str(ve)}), 401
+    except Exception as e:
+        return json.dumps({'success': False, 'exception_type': str(type(e).__name__), 'exception_details': str(e)}), 500
+
+
+
+@app.route('/delete_user/<user_id>', methods=['PUT'])
+@cross_origin()
+def delete_user(user_id):
+    """This function deletes the user along with the database data
+       associated with them.
+    """
+    try:
+        uam_object = uam(int(user_id))
+        uam_object.delete_user()
+        return json.dumps({'success': True}), 200
+    except Exception as e:
+        traceback.print_exc()
+        return json.dumps({'success': False, 'exception_type': str(type(e).__name__), 'exception_details': str(e)}), 500
+
+
+
+
+# API Calls regarding user chats.
 
 @app.route('/post_chat/<user_id>', methods=['POST'])
 @cross_origin()
@@ -76,7 +170,7 @@ def generate_response(user_id):
 @cross_origin()
 def get_chat_titles(user_id):
     try:
-        chat_data = cd(user_id)
+        chat_data = cd(int(user_id))
         chat_titles = chat_data.get_chat_titles()
         return json.dumps({'success': True, 'conversation_title': chat_titles}), 200
     except Exception as e:
@@ -117,7 +211,7 @@ def create_new_conversation(user_id):
         {'conversation_title': <conversation_title>}
     """
     try:
-        chat_data = cd(user_id)
+        chat_data = cd(int(user_id))
         json_data = request.get_json(silent=True)
         new_conv_id = chat_data.create_new_conversation(json_data['conversation_title'])
         return json.dumps({'success': True, 'new_conversation_id': new_conv_id}), 201
@@ -141,7 +235,7 @@ def update_conversation_title(user_id):
         <new_title> - The new title of the conversation to rename.
     """
     try:
-        chat_data = cd(user_id)
+        chat_data = cd(int(user_id))
         json_data = request.get_json(silent=True)
         conversation_id = json_data['conversation_id']
         new_title = json_data['new_title']
@@ -149,14 +243,40 @@ def update_conversation_title(user_id):
         return json.dumps({'success': True}), 200
     except Exception as e:
         traceback.print_exc()
-        return json.dumps({'succes': False, 'exception_details': str(e)}), 500
+        return json.dumps({'success': False, 'exception_type': str(type(e).__name__),'exception_details': str(e)}), 500
+    
+@app.route('/delete_chat/period/<user_id>', methods=['POST'])
+@cross_origin
+def delete_chat_by_period(user_id):
+    try:
+        chat_data = cd(int(user_id))
+        json_data = request.get_json(silent=False)
+        period_type = json_data['period_type']
+        period = json_data['period']
+        chat_data.delete_chat_by_period(period_type, period)
+        return json.dumps({'success': True}), 200
+    except Exception as e:
+        traceback.print_exc()
+        return json.dumps({'success': False, 'exception_type': str(type(e).__name__),'exception_details': str(e)}), 500     
+
+@app.route('/delete_chat/all/<user_id>', methods=['PUT'])
+@cross_origin
+def delete_entire_chat(user_id):
+    try:
+        chat_data = cd(int(user_id))
+        chat_data.delete_entire_chat()
+        return json.dump({'success': True}), 200
+    except Exception as e: 
+        traceback.print_exc()
+        return json.dumps({'success': False, 'exception_type': str(type(e).__name__),'exception_details': str(e)}), 500     
+
 
 @app.route('/get_chat/<user_id>', methods=['GET'])
 @cross_origin()
 def get_chat(user_id):
     
     if request.method == "GET":
-        chat_data = cd(user_id)
+        chat_data = cd(int(user_id))
         chat_logs = chat_data.return_chat_history()
         chat_data.close_conn()
         return json.dumps({'success': True, 'chat_history': chat_logs, 'user_id': int(user_id)}), 200
