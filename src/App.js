3b8efd3df4c5a97229b1df6e47cd431c6d3321ca
David Chavarro <David.e.chavarro@outlook.com>
2025-04-05 02:25:40 -0500

    Updated site to include sunset message.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import React, { useState } from "react";
 import MainNavBar from "./components/MainNavBar.js";
-import UserNavBar from "./components/UserNavBar";
 import HomePage from "./components/HomePage.js";
 import Contact from "./components/Contact.js";
 import "./App.css";
-import { Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route,  } from "react-router-dom";
 
 function App() {
   //const [userId, setUserId] = useState(-1);
-  const location = useLocation();
 
 
   return (
